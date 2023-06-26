9c36133530148432d06ac3ddba5e937427275f66
Byungho <bryanho0470@gmail.com>
2023-06-26 23:25:59 +0900

    20230626

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
+const colorOption = Array.from(document.getElementsByClassName("color-option"))
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 canvas.width = 600;
 canvas.height = 600;
+let isPainting = false;
+ctx.lineWidth = lineWidth.value;
 
-ctx.fillRect(210, 200, 15, 100);
-ctx.fillRect(350, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
-ctx.arc(290, 150, 40, 0, 2 * Math.PI);
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY)
+    ctx.stroke();
+    return;
+  }
+  coordinate = ctx.moveTo (event.offsetX, event.offsetY)
+}
 
-ctx.beginPath();
-ctx.fillStyle = "yellow";
-ctx.arc(270, 150, 5, 3.14, 2 * Math.PI); // angle is pie.
-ctx.arc(310, 150, 5, 3.14, 2 * Math.PI);
-ctx.fill();
+function onStart () {
+  isPainting = true;
+}
 
+function onStop () {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onlinewidthChange (event) {
+  ctx.lineWidth = event.target.value; 
+}
+
+function changeColor (event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function colorClicked (event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;    
+  color.value = colorValue;
+}
+
+canvas.addEventListener("mousemove", onMove)
+canvas.addEventListener("mousedown", onStart)
+canvas.addEventListener("mouseup", onStop)
+canvas.addEventListener("mouseleave", onStop)
+lineWidth.addEventListener("change", onlinewidthChange)
+color.addEventListener("change", changeColor)
+colorOption.forEach(color => color.addEventListener("click", colorClicked)); // add eventListner to all color in color Array
